1e869da978c81756eabe38a9aabd4a0118ead472
aalmajid98 <aalmajid98@gmail.com>
2023-03-26 15:29:52 -0500

    updated scrpt.js

--- public/script.js
+++ public/script.js
@@ -4,13 +4,14 @@ var bttn = document.getElementById("trSubmit");
 
 google.load("language", "1")
 function translate(name) {
-    google.language.translate(name, "en","fr", function(output) {
-    alert(output.translation)
-    })
+    google.language.translate(name, "en","ar", function(output) {
+    alert(output.translation);
+    });
 }
 google.setOnLoadCallback(translate(userText));
-
-bttn.onclick = translate(userText);
+bttn.onclick = function() {
+    translate(userText.value);
+};
 
 //typing animation
 const text = document.querySelector(".typeWord");
@@ -29,3 +30,21 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 6000);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const getDataBtn = document.getElementById("getData");
+    const resultDiv = document.getElementById("result");
+
+    if (getDataBtn) {
+        getDataBtn.addEventListener("click", async() => {
+           try {
+               const response = await fetch("http://localhost:3000/api/data");
+               const data = await response.json();
+               resultDiv.innerHTML = data.message;
+            }  catch (error) {
+                console.log("Error fetching data:", error);
+                resultDiv.textContent = "Error fetching data";
+            }
+        });
+    }
+});
